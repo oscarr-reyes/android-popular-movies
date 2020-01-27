207de30171713d661373ed799b0c542494303f83
Oscar Reyes <oscarr.reyes17@gmail.com>
2020-01-27 16:49:33 -0500

    + Added TODOs for the first stage of the project
    * Added TODOs in MainActivity class which explains the requirements for the first stage

--- app/src/main/java/dev/oscarreyes/popularmovies/MainActivity.java
+++ app/src/main/java/dev/oscarreyes/popularmovies/MainActivity.java
@@ -4,6 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/*
+* Common project requirements
+*/
+// TODO: Replace ConstraintLayout with a GridLayout in the main activity view
+// TODO: Add a spinner in the main view for loading feedback
+// TODO: Create Detail activity for selecting a movie item
+// TODO: Add title, release date, movie poster, vote average, and plot synopsis for Detail activity
+
+/*
+* User interface requirements
+*/
+// TODO: View gets updated correctly when an user changes the sort criteria
+// TODO: Movie details is started when a poster thumbnail is selected
+
+/*
+* Network API implementation
+*/
+// TODO: app queries the /movie/popular or /movie/top_rated API for the sort criteria in settings menu
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
